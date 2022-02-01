0036bce5606c56eea8d8fbaa9a4b612f4dcd779f
Przemysław Kalinowski <przemekk922@gmail.com>
2022-02-01 19:22:52 +0100

    Solve edition and reloading bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./index.css";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
@@ -6,136 +6,132 @@ import FilterButton from "./components/FilterButton";
 import Footer from "./components/Footer";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [filter, setFilter] = useState("All");
-  const [isAllTasksDone, setIsAllTasksDone] = useState(false);
+	const [tasks, setTasks] = useState([]);
+	const [filter, setFilter] = useState("All");
+	const [isAllTasksDone, setIsAllTasksDone] = useState(false);
 
-  useEffect(() => {
-    fetch("http://localhost:7000/tasks")
-      .then((res) => res.json())
-      .then((res) => setTasks(res));
-  }, []);
+	const syncTasks = useCallback(
+		() =>
+			fetch("http://localhost:7000/tasks")
+				.then((res) => res.json())
+				.then((res) => setTasks(res)),
+		[]
+	);
 
-  function deleteTask(id) {
-    const remainingTasks = tasks.filter((task) => id !== task.id);
-    setTasks(remainingTasks);
-    fetch(`http://localhost:7000/tasks/${id}`, {
-      method: "DELETE",
-    }).then(() => console.log("deleted"));
-  }
+	useEffect(() => {
+		syncTasks();
+	}, [syncTasks]);
 
-  function completeTask(id) {
-    const updatedTasks = tasks.map((task) => {
-      if (id === task.id) {
-        fetch(`http://localhost:7000/tasks/${id}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            id: task.id,
-            name: task.name,
-            isComplete: !task.isComplete,
-          }),
-        }).then(() => console.log("checked/unchecked"));
-        return { ...task, isComplete: !task.isComplete };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-  }
-  function completeAllTasks() {
-    const isAllCompleted = tasks.every((task) => task.isComplete);
-    const completedTasks = tasks.map((task) => ({
-      ...task,
-      isComplete: !isAllCompleted,
-    }));
-    setTasks(completedTasks);
-  }
+	function deleteTask(id) {
+		const remainingTasks = tasks.filter((task) => id !== task.id);
+		setTasks(remainingTasks);
+		fetch(`http://localhost:7000/tasks/${id}`, {
+			method: "DELETE",
+		}).then(() => console.log("deleted"));
+	}
 
-  function clearCompletedTasks() {
-    const clearedTasks = tasks.filter((task) => task.isComplete === false);
-    setTasks(clearedTasks);
-  }
-  function filterTasks(task) {
-    if (filter === "Completed") {
-      return task.isComplete;
-    }
-    if (filter === "Active") {
-      return !task.isComplete;
-    }
-    return true;
-  }
-  function editTask(id, newName) {
-    const editedTaskList = tasks.map((task) => {
-      if (id === task.id) {
-        fetch(`http://localhost:7000/tasks/${id}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            id: task.id,
-            name: task.name,
-            isComplete: task.isComplete,
-          }),
-        });
+	function completeTask(id) {
+		const updatedTasks = tasks.map((task) => {
+			if (id === task.id) {
+				fetch(`http://localhost:7000/tasks/${id}`, {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						id: task.id,
+						name: task.name,
+						isComplete: !task.isComplete,
+					}),
+				}).then(() => console.log("checked/unchecked"));
+				return { ...task, isComplete: !task.isComplete };
+			}
+			return task;
+		});
+		setTasks(updatedTasks);
+	}
+	function completeAllTasks() {
+		const isAllCompleted = tasks.every((task) => task.isComplete);
+		const completedTasks = tasks.map((task) => ({
+			...task,
+			isComplete: !isAllCompleted,
+		}));
+		setTasks(completedTasks);
+	}
 
-        return { ...task, name: newName };
-      }
+	function clearCompletedTasks() {
+		const clearedTasks = tasks.filter((task) => task.isComplete === false);
+		setTasks(clearedTasks);
+	}
+	function filterTasks(task) {
+		if (filter === "Completed") {
+			return task.isComplete;
+		}
+		if (filter === "Active") {
+			return !task.isComplete;
+		}
+		return true;
+	}
 
-      return task;
-    });
-    setTasks(editedTaskList);
-  }
+	function editTask(id, newName) {
+		fetch(`http://localhost:7000/tasks/${id}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				name: newName,
+			}),
+		}).then(syncTasks);
+	}
 
-  useEffect(() => {
-    const isAllCompleted = tasks.every((task) => task.isComplete);
-    setIsAllTasksDone(isAllCompleted);
-  }, [completeTask, completeAllTasks]);
+	useEffect(() => {
+		const isAllCompleted = tasks.every((task) => task.isComplete);
+		setIsAllTasksDone(isAllCompleted);
+	}, [completeTask, completeAllTasks]);
 
-  const counterTasks = tasks.length;
-  const counterUncompletedTasks = tasks.filter(
-    (task) => task.isComplete === false
-  ).length;
+	const counterTasks = tasks.length;
+	const counterUncompletedTasks = tasks.filter(
+		(task) => task.isComplete === false
+	).length;
 
-  return (
-    <div className="app">
-      <div className="content">
-        <h1 className="header">todos</h1>
-        <div className="wrapper">
-          <Form
-            setTasks={setTasks}
-            completeAllTasks={completeAllTasks}
-            isAllTasksDone={isAllTasksDone}
-            counterTasks={counterTasks}
-            tasks={tasks}
-          />
-          <ul className="task-list">
-            {tasks.filter(filterTasks).map((task) => (
-              <Todo
-                key={task.id}
-                name={task.name}
-                id={task.id}
-                isComplete={task.isComplete}
-                deleteTask={deleteTask}
-                completeTask={completeTask}
-                editTask={editTask}
-              />
-            ))}
-          </ul>
-          <FilterButton
-            counterUncompletedTasks={counterUncompletedTasks}
-            counterTasks={counterTasks}
-            clearCompletedTasks={clearCompletedTasks}
-            setFilter={setFilter}
-            filter={filter}
-          />
-        </div>
-        <Footer />
-      </div>
-    </div>
-  );
+	return (
+		<div className="app">
+			<div className="content">
+				<h1 className="header">todos</h1>
+				<div className="wrapper">
+					<Form
+						setTasks={setTasks}
+						completeAllTasks={completeAllTasks}
+						isAllTasksDone={isAllTasksDone}
+						counterTasks={counterTasks}
+						tasks={tasks}
+					/>
+					<ul className="task-list">
+						{tasks.filter(filterTasks).map((task) => (
+							<Todo
+								key={task.id}
+								name={task.name}
+								id={task.id}
+								isComplete={task.isComplete}
+								deleteTask={deleteTask}
+								completeTask={completeTask}
+								editTask={editTask}
+							/>
+						))}
+					</ul>
+					<FilterButton
+						counterUncompletedTasks={counterUncompletedTasks}
+						counterTasks={counterTasks}
+						clearCompletedTasks={clearCompletedTasks}
+						setFilter={setFilter}
+						filter={filter}
+					/>
+				</div>
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default App;
